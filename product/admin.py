100dded15c8d2c11445d45635a69b5c2cf88789e
Sanjay <arjunsanjay0@gmail.com>
2020-11-29 13:19:30 +0530

    Brand model created.

--- product/admin.py
+++ product/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Product
+from .models import *
 
 admin.site.register(Product)
+admin.site.register(Category)
+admin.site.register(Brand)
--- product/models.py
+++ product/models.py
@@ -15,13 +15,27 @@ class Product(models.Model):
     condition = models.CharField(max_length=100, choices=CONDITION_TYPE)
     price = models.DecimalField(max_digits=10, decimal_places=5)
     created = models.DateTimeField(default=timezone.now)
+    category = models.ForeignKey('Category', on_delete=models.SET_NULL, null=True)
+    brand = models.ForeignKey('Brand', on_delete=models.SET_NULL, null=True)
 
     def __str__(self):
         return self.name
 
+
 class Category(models.Model):
     category_name = models.CharField(max_length=50)
-    image_icon = models.ImageField(upload_to="products", blank=True, null=True)
+    image_icon = models.ImageField(upload_to="product_images", blank=True, null=True)
+
+    class Meta:
+        verbose_name = 'Category'
+        verbose_name_plural = 'Categories'
 
     def __str__(self):
         return self.category_name
+
+
+class Brand(models.Model):
+    brand_name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.brand_name
