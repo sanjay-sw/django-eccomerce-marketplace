6b3013a97923d86179190f6e2031dd33da710291
Sanjay <arjunsanjay0@gmail.com>
2020-11-29 11:55:57 +0530

    Intial Commit

--- product/models.py
+++ product/models.py
@@ -2,15 +2,13 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.utils import timezone
 
+
 # Create your models here.
-
 class Product(models.Model):
-
     CONDITION_TYPE = (
         ("new", "New"),
         ("used", "Used"),
     )
-
     owner = models.ForeignKey(User, on_delete=models.CASCADE)
     name = models.CharField(max_length=100)
     description = models.TextField(max_length=500)
