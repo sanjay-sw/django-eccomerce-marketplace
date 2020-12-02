371092b895086b52b9c0bb1e4a773d5aea8f94f0
Sanjay <arjunsanjay0@gmail.com>
2020-12-03 03:39:32 +0530

    created and tested product and detail pages

--- product/views.py
+++ product/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from .models import Product
 
-# Create your views here.
+
+def productlist(request):
+    all_products = Product.objects.all()
+
+    context = {'product_list': all_products}
+    template = 'Product/product_list.html'
+    return render(request, template, context)
+
+
+def productdetail(request, key):
+    print(key)
+    detail = Product.objects.get(id=key)
+    template = 'Product/product_detail.html'
+    context = {'product_detail': detail}
+    return render(request, template, context)
